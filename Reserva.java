071684d70c174ff3d664cb0096c5f6ff5c82776d
Ramirocambre1 <39927232+Ramirocambre1@users.noreply.github.com>
2018-06-04 18:40:50 -0300

    Add files via upload

---
+++ Reserva.java
@@ -0,0 +1,48 @@
+package clases;
+
+public class Reserva {
+	
+	private int numeroReserva;
+	private String dniPasajero;
+	private int cantidadPasajeros;
+	private double costo;
+	public Reserva(int numeroReserva, String dniPasajero, int cantidadPasajeros, double costo) {
+		
+		this.numeroReserva = numeroReserva;
+		this.dniPasajero = dniPasajero;
+		this.cantidadPasajeros = cantidadPasajeros;
+		this.costo = costo;
+	}
+	
+	public void agregarPasajero(Pasajero e)
+	{
+		
+		
+	}
+	
+	public void setFechaEntrada()
+	{
+		
+	}
+	
+	public void setFechaSalida()
+	{
+		
+	}
+	
+	public void getFechaEntrada()
+	{
+		
+	}
+	
+	public void getFechaSalida()
+	{
+		
+	}
+	
+	public void calcularCostos()
+	{
+		
+	}
+
+}
