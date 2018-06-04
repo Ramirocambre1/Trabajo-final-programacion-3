be5cbcc0e9512058333fa7017f262d0a85548701
Cristian221995 <39927364+Cristian221995@users.noreply.github.com>
2018-06-04 18:55:48 -0300

    Add files via upload

---
+++ Trabajo Practico Final 3/src/clases/Fecha.java
@@ -0,0 +1,26 @@
+package clases;
+
+import java.sql.Date;
+
+public class Fecha {
+	private Date fechaIn;
+	private Date fechaOut;
+	
+	public Fecha(Date fechaIn, Date fechaOut) {
+		this.fechaIn = fechaIn;
+		this.fechaOut = fechaOut;
+	}
+	public Date getFechaIn() {
+		return fechaIn;
+	}
+	public void setFechaIn(Date fechaIn) {
+		this.fechaIn = fechaIn;
+	}
+	public Date getFechaOut() {
+		return fechaOut;
+	}
+	public void setFechaOut(Date fechaOut) {
+		this.fechaOut = fechaOut;
+	}
+
+}
