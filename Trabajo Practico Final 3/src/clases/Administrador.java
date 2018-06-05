0b35e3125f7b4a105a7cbc1284c85c2214c80e96
Cristian221995 <39927364+Cristian221995@users.noreply.github.com>
2018-06-05 09:08:51 -0300

    Add files via upload

--- Trabajo Practico Final 3/src/clases/Administrador.java
+++ Trabajo Practico Final 3/src/clases/Administrador.java
@@ -1,5 +1,18 @@
 package clases;
 
-public class Administrador {
+import java.util.ArrayList;
+
+public class Administrador extends Usuario {
+	
+	private ArrayList<Recepcionista>recepcionistas;
+
+	public Administrador(ArrayList<Recepcionista> recepcionistas, String usuario, String password) {
+		super(usuario, password);
+		this.recepcionistas = recepcionistas;
+	}
+	
+	
+	
+	
 
 }
--- Trabajo Practico Final 3/src/clases/Disponible.java
+++ Trabajo Practico Final 3/src/clases/Disponible.java
@@ -1,5 +1,30 @@
 package clases;
 
 public class Disponible {
+	private boolean disponibilidad;
+	private String detalle;
+	
+	public Disponible(boolean disponibilidad, String detalle) {
+		this.disponibilidad = disponibilidad;
+		this.detalle = detalle;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	
+	public void ocupar() {
+		disponibilidad=true;
+	}
+	
+	public void desocupar() {
+		disponibilidad=false;
+	}
+	
+	
 
 }
