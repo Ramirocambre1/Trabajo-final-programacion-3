916e0debaa262fcf9077afd20ae856fdbb755f39
Ramirocambre1 <39927232+Ramirocambre1@users.noreply.github.com>
2018-06-06 18:16:55 -0300

    Add files via upload

--- Trabajo Practico Final 3/src/clases/Reserva.java
+++ Trabajo Practico Final 3/src/clases/Reserva.java
@@ -1,25 +1,28 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Reserva {
 	
 	private int numeroReserva;
 	private String dniPasajero;
 	private int cantidadPasajeros;
 	private double costo;
+	ArrayList<Pasajero> registroPasajeros; // El array list lo saque de habitacion
 	public Reserva(int numeroReserva, String dniPasajero, int cantidadPasajeros, double costo) {
-		
+		registroPasajeros = new ArrayList<Pasajero>();
 		this.numeroReserva = numeroReserva;
 		this.dniPasajero = dniPasajero;
 		this.cantidadPasajeros = cantidadPasajeros;
 		this.costo = costo;
 	}
 	
-	public void agregarPasajero(Pasajero e)
+	public void agregarPasajero(Pasajero e) //
 	{
-		
+		registroPasajeros.add(e);
 		
 	}
-	
+	/*
 	public void setFechaEntrada()
 	{
 		
@@ -44,7 +47,7 @@ public class Reserva {
 	{
 		
 	}
-
+	*/
 }
 
 // hola lollllldsfgdgdf
